b10cec0b2c31f2b6991ea898a4aba80008d13fad
diomedescerda <kenmatwofour@gmail.com>
2025-10-15 18:07:40 -0500

    some advances in the otsu method

--- otsu.py
+++ otsu.py
@@ -1,12 +1,30 @@
 import matplotlib.pyplot as plt
-import cv2
+from histogram import grayHistogram
+import numpy as np
 
-# Read as grayscale
-img = cv2.imread('rice.jpg', cv2.IMREAD_GRAYSCALE)
+hist = grayHistogram()
 
-# Plot histogram
-plt.hist(img.ravel(), bins=256, range=(0, 256))
-plt.title('Grayscale Histogram')
-plt.xlabel('Pixel value')
+J_theta = None
+
+for i in range(hist.flatten().shape[0]):
+    p_0 = hist[:i].sum() / hist.sum()
+    p_1 = hist[i:].sum() / hist.sum()
+
+    if (len(hist[:i]) == 0) or (len(hist[i:]) == 0):
+        continue
+
+    std_0 = np.std(hist[:i])
+    std_1 = np.std(hist[i:])
+
+    J = p_0 * (std_0 ** 2) + p_1 * (std_1 ** 2)
+
+    if J_theta is None or J > J_theta:
+        J_theta = J
+        theta = i
+
+plt.plot(hist)
+plt.axvline(x=theta, color='r', linestyle='--', label=f'Theta = {theta}')
+plt.xlabel('Pixel Value')
 plt.ylabel('Frequency')
+plt.legend()
 plt.show()
