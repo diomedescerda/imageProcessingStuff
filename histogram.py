e9f6a53264c8b17af4a62a9862a115adb4094031
diomedescerda <kenmatwofour@gmail.com>
2025-10-15 16:54:53 -0500

    gray histogram working

---
+++ histogram.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def grayHistogram():
+    root = os.getcwd()
+    imgPath = os.path.join(root, 'rice.jpeg')
+    if not os.path.exists(imgPath):
+        print(f"Error: image not found at {imgPath}")
+        return
+
+    img = cv.imread(imgPath, cv.IMREAD_GRAYSCALE)
+    if img is None:
+        print("Error: Could not load image")
+        return
+
+    plt.figure()
+    plt.imshow(img, cmap='gray')
+
+    hist = cv.calcHist([img], [0], None, [256], [0,256])
+
+    plt.figure()
+    plt.plot(hist)
+    plt.xlabel('bins')
+    plt.ylabel('# of pixels')
+    plt.show()
+
+if __name__ == '__main__':
+    grayHistogram()
