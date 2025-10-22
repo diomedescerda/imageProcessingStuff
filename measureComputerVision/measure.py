f7c704632f078ae27e06c1e09b64c18bdc08a6ae
diomedescerda <kenmatwofour@gmail.com>
2025-10-22 15:00:13 -0500

    mm per pixel already on the pocket

--- measureComputerVision/measure.py
+++ measureComputerVision/measure.py
@@ -17,6 +17,28 @@ def apply_smoothing(gray, method="gaussian", ksize=5):
         return cv.bilateralFilter(gray, d=ksize, sigmaColor=75, sigmaSpace=75)
     return cv.GaussianBlur(gray, (ksize, ksize), 0)
 
+def get_mm_per_pixel(img, contours, ref_size_mm=23.7):
+    display_img = img.copy()
+    cv.drawContours(display_img, contours, -1, (0,255,0), 3)
+
+    circularity = 0
+    best_contour = None
+    for i, contour in enumerate(contours):
+        area = cv.contourArea(contour)
+        perimeter = cv.arcLength(contour, True)
+        
+        if perimeter > 300:
+            temp = (4 * np.pi * area) / (perimeter ** 2)
+            if temp > circularity:
+                circularity = temp
+                best_contour = contour
+
+    area = cv.contourArea(best_contour)
+    diameter_pixels = 2 * np.sqrt(area / np.pi)
+    mm_per_pixel = ref_size_mm / diameter_pixels
+
+    return mm_per_pixel
+
 def measure_main(imgName, ref_size_mm=23.7):
     root = os.getcwd()
     imgPath = os.path.join(root, imgName)
@@ -37,12 +59,14 @@ def measure_main(imgName, ref_size_mm=23.7):
     eroded = cv.erode(binary, kernel, iterations=3)
     dilated = cv.dilate(eroded, kernel, iterations=2)
 
-    contours = cv.findContours(dilated, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+    contours = cv.findContours(dilated, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[0]
     if not contours:
         print("No contours found")
         return
 
-    cv.drawContours(img, contours, -1, (0,255,0), 3)
+    mm_per_pixel = get_mm_per_pixel(img, contours, ref_size_mm)
+    print(f"Millimeters per pixel: {mm_per_pixel}")
+    
 
 if __name__ == "__main__":
     measure_main("example.jpeg")
